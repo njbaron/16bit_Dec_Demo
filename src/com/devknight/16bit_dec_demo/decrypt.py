afe4c983d8642d7407816b9b52494c710eff524d
Nicholas Baron <njbaron@users.noreply.github.com>
2018-01-24 21:30:35 -0700

    Added comments

--- src/com/devknight/16bit_dec_demo/decrypt.py
+++ src/com/devknight/16bit_dec_demo/decrypt.py
@@ -1,19 +1,26 @@
+# Author: Nicholas Baron (830278807)
+# Description: This program is a 16bit decrypter with an 8 bit key. It takes 16bits divides it into
+# 2 bytes, a and b. The bytes are then decrypted with the function (a,b) = (b(XOR)key,a).s
+
 import sys
 import os
 
 debug = False
 
+
 def getKeys(keyFileName, numberKeys = 8):
     keys = read(keyFileName)
 
-    if(len(keys)>numberKeys):
+    if len(keys) > numberKeys:
         print("[WARNING] Found more than " + numberKeys + " keys.")
         print("[NOTICE] Only using " + numberKeys + " keys.")
 
     return keys[:numberKeys]
 
+
 def decrypt(key, inString):
-    print("[LOG} Decrypt with key: " + chr(key))
+    if debug:
+        print("[LOG} Decrypt with key: " + chr(key))
     outString = bytearray()
     inString = bytearray(inString)
 
@@ -26,26 +33,43 @@ def decrypt(key, inString):
 
     return outString
 
+
 def read(file):
-    print("[LOG] Reading file: " + file)
+    if debug:
+        print("[LOG] Reading file: " + file)
     f = open(file, "rb")
     retStr = f.read()
     f.close()
     return retStr
 
+
 def write(file, string):
-    print("[LOG] Writing file: " + file)
+    if debug:
+        print("[LOG] Writing file: " + file)
     f = open(file, "wb")
     f.write(string)
     f.close()
 
 
+# Main Program
+if sys.argv[1] == "-d":
+    debug = True
+    i = 1
+else:
+    debug = False
+    i = 0
+
 print("--Welcome to 16bit DEC decrypter.--")
+
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-keyFile = os.path.join(THIS_FOLDER, sys.argv[1])
-inFile = os.path.join(THIS_FOLDER, sys.argv[2])
+keyFile = os.path.join(THIS_FOLDER, sys.argv[1 + i])
+inFile = os.path.join(THIS_FOLDER, sys.argv[2 + i])
+
 keys = getKeys(keyFile)
 string = read(inFile)
+
 for i in range(len(keys)-1, -1, -1):
     string = decrypt(keys[i], string)
 write(inFile+"_d", string)
+
+print("--Decryption Done--")
--- src/com/devknight/16bit_dec_demo/encrypt.py
+++ src/com/devknight/16bit_dec_demo/encrypt.py
@@ -1,17 +1,22 @@
+# Author: Nicholas Baron (830278807)
+# Description: This program is a 16bit encrypter with an 8 bit key. It takes 16bits divides it into
+# 2 bytes, a and b. The bytes are then encrypted with the function (a,b) = (b,a(XOR)key).
+
 import sys
 import os
 
 debug = False
-i = 0
+
 
 def getKeys(keyFileName, numberKeys = 8):
     keys = read(keyFileName)
 
-    if(len(keys)>numberKeys):
+    if len(keys) > numberKeys:
         print("[WARNING] Found more than " + numberKeys + " keys.")
         print("[NOTICE] Only using " + numberKeys + " keys.")
 
     return keys[:numberKeys]
+
 
 def encrypt(key, inString):
     if debug:
@@ -28,6 +33,7 @@ def encrypt(key, inString):
 
     return outString
 
+
 def read(file):
     if debug:
         print("[LOG] Reading file: " + file)
@@ -35,6 +41,7 @@ def read(file):
     retStr = f.read()
     f.close()
     return retStr
+
 
 def write(file, string):
     if debug:
@@ -44,21 +51,25 @@ def write(file, string):
     f.close()
 
 
-try:
-    if sys.argv[1] == "-d":
-        debug = True
-        i = 1
-except:
+# Main Program
+if sys.argv[1] == "-d":
+    debug = True
+    i = 1
+else:
     debug = False
     i = 0
 
-if debug:
-    print("--Welcome to 16bit DEC encrypter.--")
+print("--Welcome to 16bit DEC encrypter.--")
+
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 keyFile = os.path.join(THIS_FOLDER, sys.argv[1 + i])
 inFile = os.path.join(THIS_FOLDER, sys.argv[2 + i])
+
 keys = getKeys(keyFile)
 string = read(inFile)
+
 for i in range(0, len(keys)):
     string = encrypt(keys[i], string)
 write(inFile+"_e", string)
+
+print("--Encryption Done--")
